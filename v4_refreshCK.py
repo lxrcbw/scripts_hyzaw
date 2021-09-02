7d4d72ac98f934667448a5cc0e0c1859c853f2c0
hyzaw <62041055+hyzaw@users.noreply.github.com>
2021-09-02 20:17:05 +0800

    Update v4_refreshCK.py

--- v4_refreshCK.py
+++ v4_refreshCK.py
@@ -14,9 +14,8 @@ def getconfig():
 
 def wstopt(wskey):
     try:
-        url = "http://cdn.xia.me/getck"
+        url = "https://signer.nz.lu/getck"
         headers = {
-            "Host": "signer.nz.lu",
             "user-agent": "Mozilla/5.0 (Windows NT 6.3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"
         }
         data = {"wskey": wskey, "key": "xb3z4z2m3n847"}
